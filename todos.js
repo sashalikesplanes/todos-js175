9092ba7456129dcf0dc5fd1a29a5933e029195fc
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-30 20:59:43 +0200

    add ability to add new lists

--- todos.js
+++ todos.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
 const port = 3000;
 
 let todoLists = require("./lib/seed-data");
+const { urlencoded } = require("express");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 const compareByTitle = (listA, listB) => {
   const titleA = listA.title.toLowerCase();
@@ -32,7 +35,21 @@ const sortTodoLists = (lists) => {
 };
 
 app.get("/", (req, res) => {
+  res.redirect("/lists");
+});
+
+app.get("/lists", (req, res) => {
   res.render("lists", { todoLists: sortTodoLists(todoLists) });
+});
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+app.post("/lists", (req, res) => {
+  const title = req.body.todoListTitle.trim();
+  todoLists.push(new TodoList(title));
+  res.redirect("/lists");
 });
 
 app.listen(port, host, () => console.log(`Listening on ${port} of ${host}`));
