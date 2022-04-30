b43f39bc12e3a446a13bd2e0a308d07427f4de5b
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-30 20:05:55 +0200

    initial app setup/boilerplate

---
+++ todos.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.render("lists");
+});
+
+app.listen(port, host, () => console.log(`Listening on ${port} of ${host}`));
