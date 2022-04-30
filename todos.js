7ac2b68bea77ed2a77d6023f93de29db64690692
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-30 20:16:11 +0200

    made page to show lack of todos

--- todos.js
+++ todos.js
@@ -5,13 +5,16 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
+let todoLists = require("./lib/seed-data");
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("lists");
+  res.render("lists", { todoLists });
 });
 
 app.listen(port, host, () => console.log(`Listening on ${port} of ${host}`));
